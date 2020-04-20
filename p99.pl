85e85ae5b00430f9f0f5c16bc5243c0e6b40c68b
Jens Schauder <jens@schauderhaft.de>
2020-04-20 19:04:46 +0200

    added tests.

--- p99.pl
+++ p99.pl
@@ -1,4 +1,19 @@
 my_last(X, [X]).
 
-my_last(X, [Y|YS]) :-
+my_last(X, [_|YS]) :-
   my_last(X, YS).
+
+
+:- begin_tests(problems_99_1_last_element_of_a_list).
+
+test(empty_list, fail) :-
+  my_last(_, []).
+
+test(full_list, true(X = e), nodet) :-
+  my_last(X, [b, c, d, e]).
+
+test(finds_lists_with_trailing_constant, true(L = [_, _, _, a]), nodet) :-
+  length(L, 4),
+  my_last(a, L).
+
+:- end_tests(problems_99_1_last_element_of_a_list).
